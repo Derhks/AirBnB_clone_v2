f595d9bc4f53c2a15cd1cd0b17f18a704a71dc7a
Derhks <derhks@gmail.com>
2020-04-11 23:24:32 +0000

    [Fix] output the verification

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,8 @@ import os
 from sys import argv
 
 env.hosts = ['35.231.33.237', '35.185.107.146']
-env.user = argv[7]
-env.password = argv[5]
+# env.user = argv[7]
+# env.password = argv[5]
 
 
 def do_pack():
@@ -28,18 +28,19 @@ def do_deploy(archive_path):
             remote_path = "/tmp/{:s}".format(archive_path[9:])
             path = "web_static/releases"
             put(archive_path, remote_path)
-            run("mkdir -p /data/web_static/releases/{:s}".
+            run("mkdir -p /data/web_static/releases/{:s}/".
                 format(archive_path[9:-4]))
-            run("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}".
+            run("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}/".
                 format(archive_path[9:], archive_path[9:-4]))
             run("rm /tmp/{:s}".format(archive_path[9:]))
-            run("mv /data/{:s}/{:s}/web_static/* /data/{:s}/{:s}"
+            run("mv /data/{:s}/{:s}/web_static/* /data/{:s}/{:s}/"
                 .format(path, archive_path[9:-4], path, archive_path[9:-4]))
             run("rm -rf /data/web_static/releases/{:s}/web_static".
                 format(archive_path[9:-4]))
             run("rm -rf /data/web_static/current")
             run("ln -s /data/{:s}/{:s}/ /data/web_static/current".
                 format(path, archive_path[9:-4]))
+            print("New version deployed!")
             return True
         except:
             return False
